75f8cb04d53be69dcb4fa7dcecdb06f7db06c297
Ray Mavrovich <rmavrovi@buffalo.edu>
2025-12-20 13:00:22 -0500

    unindentation 360

--- projects/project-4/assignment/src/scripts/measure_rdflib.py
+++ projects/project-4/assignment/src/scripts/measure_rdflib.py
@@ -357,7 +357,7 @@ for _, row in df.iterrows():
     graph.add((reading_uri, NS_CCO.ont00001904, mice_uri))
     graph.add((mice_uri, NS_EXPROP.hasTimestamp, Literal(timestamp_raw, datatype=XSD.dateTime)))
 ensure_clean_definition(c, definition)
-    NS_CCO.ont00000441,
+NS_CCO.ont00000441,
 )
 graph.add((NS_CCO.ont00000441, RDFS.subClassOf, NS_OBO.BFO_0000020))
 graph.add((NS_CCO.ont00000995, RDFS.subClassOf, NS_OBO.BFO_0000040))
